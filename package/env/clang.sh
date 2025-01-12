9802a4f67d228d0da31c6d1520637dad9415e378
kod1ngclub <wiiwefwefbi@gmail.com>
2025-01-12 21:00:50 +0900

    FEAT: Add more clang tools - make - valgrind

--- package/env/clang.sh
+++ package/env/clang.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# install clang
 sudo apt install -y clang
+
+# install make & valgrind (debug)
+sudo apt install -y make
+sudo apt install -y valgrind
+
+# install clangd (LSP)
 sudo apt install -y clangd
