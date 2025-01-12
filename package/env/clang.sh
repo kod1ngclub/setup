a417dcec709f8bb7cf969a5cd9bc9b6d85b7d38d
kod1ngclub <wiiwefwefbi@gmail.com>
2025-01-13 00:17:59 +0900

    FEAT: Add gprof & perf

--- package/env/clang.sh
+++ package/env/clang.sh
@@ -3,9 +3,13 @@
 # install clang
 sudo apt install -y clang
 
-# install make & valgrind (debug)
+# install make (build tool)
 sudo apt install -y make
+
+# install valgrind & gprof & perf
 sudo apt install -y valgrind
+sudo apt install -y gprof
+sudo apt-get install linux-tools-common linux-tools-generic
 
 # install clangd (LSP)
 sudo apt install -y clangd
