db4861c185f0f2b3befa41cc89677f4a1fecfa66
kod1ngclub <wiiwefwefbi@gmail.com>
2025-01-30 18:46:48 +0900

    Set repository download path

--- install.sh
+++ install.sh
@@ -4,24 +4,23 @@
 sudo apt update
 sudo apt upgrade -y
 
-# install package/util
+# ==== install packages
 source package/util/zip.sh
 source package/util/unzip.sh
 source package/util/curl.sh
 source package/util/tar.sh
 
-# install package/tool
 source package/tool/nvim.sh
 source package/tool/code.sh
 
-# intall package/env
 source package/env/clang.sh
 source package/env/node.sh
-source package/env/java.sh
 
-# clone repos from github
-git clone https://github.com/kod1ngclub/kod1ngclub.git
-git clone https://github.com/kod1ngclub/howc.git
-git clone https://github.com/kod1ngclub/ctl.git
-git clone https://github.com/kod1ngclub/ccli.git
+# ==== clone repositories
+mkdir $HOME/repos
+
+git clone https://github.com/kod1ngclub/kod1ngclub.git      $HOME/repos/kod1ngclub
+git clone https://github.com/kod1ngclub/howc.git            $HOME/repos/howc
+git clone https://github.com/kod1ngclub/ctl.git             $HOME/repos/ctl
+git clone https://github.com/kod1ngclub/ccli.git            $HOME/repos/ccli
 
--- package/env/node.sh
+++ package/env/node.sh
@@ -6,9 +6,6 @@ sudo apt install -y curl
 # install FNM
 curl -fsSL https://fnm.vercel.app/install | bash
 
-# apply FNM
-source $BASHRC
-
-# install node v22
-fnm install 22
-fnm use 22
+# install node v23
+fnm install 23
+fnm use 23
