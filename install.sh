57970028809af5b5ef9e96e5cd21a169a29001f6
kod1ngclub <wiiwefwefbi@gmail.com>
2025-02-07 15:17:08 +0900

    Rename package/tool to package/ide

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ source package/util/unzip.sh
 source package/util/curl.sh
 source package/util/tar.sh
 
-source package/tool/nvim.sh
-source package/tool/code.sh
+source package/ide/nvim.sh
+source package/ide/code.sh
 
 source package/env/clang.sh
 source package/env/node.sh
